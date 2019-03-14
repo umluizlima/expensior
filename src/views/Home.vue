2a67943fd5e3a6fbea27e2aef6f0a14fb62d4d83
umluizlima <umluizlima@gmail.com>
2019-03-13 21:17:34 -0300

    Add form buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="home">
+  <div id="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <EntryForm/>
+    <button class="button is-primary is-large is-fullwidth"
+      @click="isPileModalActive = true">
+      New pile
+    </button>
+
+    <b-modal :active.sync="isPileModalActive" canCancel="false" has-modal-card>
+      <PileForm/>
+    </b-modal>
+
+    <button class="button is-primary is-large is-fullwidth"
+      @click="isEntryModalActive = true">
+      New entry
+    </button>
+
+    <b-modal :active.sync="isEntryModalActive" canCancel="false" has-modal-card>
+      <EntryForm/>
+    </b-modal>
   </div>
 </template>
 
@@ -10,12 +26,33 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
 import EntryForm from '@/components/EntryForm.vue';
+import PileForm from '@/components/PileForm.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
     EntryForm,
+    PileForm,
+  },
+  data() {
+    return {
+      isEntryModalActive: true,
+      isPileModalActive: false,
+    };
   },
 };
 </script>
+
+<style>
+  #home {
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    text-align: left;
+  }
+</style>
